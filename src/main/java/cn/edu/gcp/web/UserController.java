43c66c5760fcc07613a036ece351fe67631259c5
陈俊华 <1109551489@qq.com>
2017-09-11 11:06:46 +0800

    调整日志

--- src/main/java/cn/edu/gcp/web/UserController.java
+++ src/main/java/cn/edu/gcp/web/UserController.java
@@ -2,6 +2,8 @@ package cn.edu.gcp.web;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,7 @@ import cn.edu.gcp.service.UserService;
 @Controller
 @RequestMapping("user")
 public class UserController {
-	//private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private UserService userService;
@@ -21,6 +23,7 @@ public class UserController {
 	@RequestMapping("list")
 	public String list(Model model) {
 		List<User> list=userService.getList();
+		logger.debug("list.size" ,list.size());
 		//System.out.println(list.size());
 		model.addAttribute("list", list);
 		return "list";
